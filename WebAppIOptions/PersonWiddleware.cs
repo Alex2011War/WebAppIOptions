43305e0319be657405da2e823474eeb6ce62616d
alexei.vakulencko@yandex.ru <alexei.vakulencko@yandex.ru>
2022-01-11 18:19:32 +0400

    Импорт конфигурации через объект IOptions

---
+++ WebAppIOptions/PersonWiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace WebAppIOptions
+{
+    public class PersonWiddleware
+    {
+        private readonly RequestDelegate next;
+        public Person Person { get; }
+
+        public PersonWiddleware(RequestDelegate _next, IOptions<Person> options)
+        {
+            next = _next;
+            Person = options.Value;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"<p>Name:{Person?.Name}</p>");
+            stringBuilder.Append($"<p>Age:{Person.Age}</p>");
+            stringBuilder.Append($"<p>Age:{Person.Company.Title}</p>");
+            stringBuilder.Append("<h3>Languages</h3><ul>");
+            foreach (var lang in Person.Languages)
+            
+                stringBuilder.Append($"<li>{lang}</li>");
+                stringBuilder.Append("</ul>");
+            
+
+            await context.Response.WriteAsync(stringBuilder.ToString());
+
+        }
+    }
+}
--- WebAppIOptions/Startup.cs
+++ WebAppIOptions/Startup.cs
@@ -5,30 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebAppIOptions
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration AppConfiguration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("person.json");
+            AppConfiguration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<Person>(AppConfiguration);
+            services.Configure<Person>(opt => { opt.Age = 33; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMiddleware<PersonWiddleware>();
+
         }
     }
 }
